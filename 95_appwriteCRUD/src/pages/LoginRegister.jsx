fdb44501571556215ef352880d7e5c960a09501a
Andre Krug <andre.krug@outlook.de>
2024-06-21 11:42:25 +0200

    Projekte getestet

---
+++ 95_appwriteCRUD/src/pages/LoginRegister.jsx
@@ -0,0 +1,10 @@
+
+function LoginRegister() {
+  return (
+    <div>
+      <h1>Login / Register</h1>
+    </div>
+  )
+}
+
+export default LoginRegister
---
+++ 95_appwriteCRUD/src/pages/Notes.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState  } from "react"
+import { databases } from "../appwrite/config"
+
+
+
+function Notes() {
+  
+  const [notes, setNotes] = useState ([]);
+ useEffect(()=> {
+    init()
+ }, [])
+
+  const init = async () => {
+        const response = await databases.listDocuments (
+            
+            import.meta.env.VITE_DATABASE_ID,
+            import.meta.env.VITE_COLLECTION_ID_NOTES
+        );
+
+        setNotes (response.documents);
+  };
+
+
+    return (
+    <div>
+      {notes.map((note)=>(
+        <div key={note.$id}>{note.body}</div>
+      ))}
+    </div>
+  )
+}
+
+export default Notes
